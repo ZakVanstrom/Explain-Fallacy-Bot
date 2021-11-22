d7cce8a06d6ad4ed141c1469fe027c72f2d20240
Zak Vanstrom <zvanstro@asu.edu>
2021-11-22 02:42:41 -0700

    added more fallacies

--- src/explain_fallacy.py
+++ src/explain_fallacy.py
@@ -13,11 +13,31 @@ def start_handling(app, msg):
 
 class ExplainFallacy:
     fallacy_dict = {
+        # '': {
+        #     'title': "",
+        #     'wikipedia': "[]()",
+        #     'summary': ""
+        # },
         'false equivalence': {
             'title': "False Equivalence",
             'wikipedia': "[False Equivalence](https://en.wikipedia.org/wiki/False_equivalence)",
             'summary': ">False equivalence is a logical fallacy in which an equivalence is drawn between two subjects based on flawed or false reasoning. This fallacy is categorized as a fallacy of inconsistency. Colloquially, a false equivalence is often called 'comparing apples and oranges'."
-        }
+        },
+        'red herring': {
+            'title': 'Red Herring',
+            'wikipedia': '[Red Herring](https://en.wikipedia.org/wiki/Red_herring)',
+            'summary': 'A red herring is something that misleads or distracts from a relevant or important question. It may be either a logical fallacy or a literary device that leads readers or audiences toward a false conclusion. A red herring may be used intentionally, as in mystery fiction or as part of rhetorical strategies (e.g., in politics), or may be used in argumentation inadvertently.'
+        },
+        'appeal to pity': {
+            'title': 'Appeal to Pity',
+            'wikipedia': '[Appeal to Pity](https://en.wikipedia.org/wiki/Appeal_to_pity)',
+            'summary': "An appeal to pity is a fallacy in which someone tries to win support for an argument or idea by exploiting his opponent's feelings of pity or guilt. It is a specific kind of appeal to emotion. The name 'Galileo argument' refers to the scientist's suffering as a result of his house arrest by the Inquisition."
+        },
+        'appeal to authority': {
+            'title': "Appeal to Authority",
+            'wikipedia': "[Appeal to Authority](https://en.wikipedia.org/wiki/Argument_from_authority)",
+            'summary': "An argument from authority is a form of argument in which the opinion of an authority on a topic is used as evidence to support an argument. Some consider that it is used in a cogent form if all sides of a discussion agree on the reliability of the authority in the given context, and others consider it to always be a fallacy to cite the views of an authority on the discussed topic as a means of supporting an argument."
+        },
     }
     response = ""
     fallacy = ""
@@ -59,7 +79,7 @@ class ExplainFallacy:
         self.response += "https://en.wikipedia.org/wiki/Fallacy) of " + self.info['wikipedia'] + "\n\n"
         self.response += "**" + self.info['title'] + "** \n\n"
         self.response += self.info['summary'] + "\n\n"
-        self.response += "^(\[ Fallacy List |) [^(GitHub)](https://github.com/ZakVanstrom/Explain-Fallacy-Bot) ^(| v0.0.0 | Want a Fallacy added?)  [^(Contact Me)](mailto:zvanstro@asu.edu) ^(\])"
+        self.response += "^(\[) [^(Fallacy List)](https://github.com/ZakVanstrom/Explain-Fallacy-Bot/blob/master/fallacy_list.md) ^(|) [^(GitHub)](https://github.com/ZakVanstrom/Explain-Fallacy-Bot) ^(| v0.0.0 | Want a Fallacy added?)  [^(Contact Me)](https://www.patreon.com/zak_vanstrom?fan_landing=true) ^(\])"
 
     def respond_to_message(self):
         self.msg.parent().reply(self.response)
